0b326f2cbacce490b56a0a5e25f5a599948731f5
klangwig <klangwig@gmail.com>
2022-07-27 22:08:09 -0400

    updated code

--- data_management/data_management_tidyverse.R
+++ data_management/data_management_tidyverse.R
@@ -1,5 +1,5 @@
 
-###-----tidyverse------###
+####---------------------tidyverse----------------------####
 
 # the tidyverse is a powerful set of seperate packages
 # with functions that make working with data in R much easier
@@ -11,12 +11,181 @@
 #remove the hashtag to install the packages
 #install.packages('tidyverse')
 
-#load the tidyverse package
+# load the tidyverse package
 library(tidyverse)
 
-#read in two datasets
+# read in two datasets
 # you will need to tell R where to find them on your computer
+# I am using an .Rproj file, which sets my working directory within
+# the working directory
 
-bat.inf = read.csv("bat_data.csv")
+batdat = read.csv("data_management/bat_data.csv")
+# in this dataset, each row is a bat that 
+# is positive or negative for P. destructans
 
-bat.count = read.csv("count_data.csv")
+batcount = read.csv("data_management/bat_count.csv")
+# in this dataset, each row the count of hibernation site
+# for a partcular species on a particular date
+# even though sites were counted on the same date
+# species are repeated across dates
+
+# my favorite tidyverse command - group_by!
+# group_by is kind of like using excels version of filter
+# to create small a small dataset that is grouped by the variables
+# for example, if I group_by species, date
+# this is the equivalent in excel of selecting a species (e.g. MYLU)
+# and a specific date (e.g. March 1 2016, 3/1/16)
+# except group_by does this for every species and date combination in your dataset
+# this is incredibly useful because in other programs, you might need
+# to write a for loop to have this capability
+
+## Group by, Mutate, and Summarise
+
+#first add a column to the infection data that is the log10 of fungal loads
+batdat$lgdL = log10(batdat$gdL)
+
+# Summarise
+batdat %>% 
+  group_by(species) %>% 
+  summarise(mean.fungal.loads=mean(lgdL,na.rm=TRUE))
+# this gives you a summary table, it doesn't change batdat!
+
+# if you want to call this table something you would need to assign it
+# when using summarise, you always want to call your summary table something different
+# this is like making a pivot_table in excel
+
+fungal.load.table = batdat %>% 
+  group_by(species) %>% 
+  summarise(mean.fungal.loads=mean(lgdL,na.rm=TRUE))
+
+fungal.load.table
+
+## Summarise versus Mutate
+
+# Mutate adds a column
+# when mutating, you can just call the object you are making
+# the same thing as your original dataframe
+# because you are adding a column based on your
+# original dataframe
+batdat = batdat %>% 
+  #replace batdat with a new dataframe that has something you want
+  group_by(site,species,date) %>% 
+  #you can group_by multiple things
+  mutate(sample.size=n())
+#this adds a column to the dataframe
+batdat
+
+## Joining
+# joining datasets together is a useful skill
+# especially if we have two datasets we need to match on a specific column
+# or set of columns
+# https://dplyr.tidyverse.org/reference/mutate-joins.html
+
+# lets join add the bat count data with the infection data
+
+# always call your new dataframe something new
+# don't write over and old dataframe in case you make
+# a mistake joining
+
+#inner_join(): includes all rows in x and y.
+#when inner joining, non-matching rows will be dropped!
+
+#left_join(): includes all rows in x.
+# when left joining, every row in x is kept, but only those matching
+# x are kept in y
+
+#right_join(): includes all rows in y.
+# when right joining, every row in y is kept, but only those matching
+# y are kept in x
+
+
+#full_join(): includes all rows in x or y.
+# every row is kept in both x and y
+
+batdat_count = left_join(
+  x = batdat,
+  y = batcount,
+  by = c("site","species","date")
+)
+
+View(batdat_count)
+# now, every row in batdat remains, and we have merged in counts
+
+# comparing to right_join
+# using the fungal load table we create above
+
+count_fungal = right_join(
+  x = fungal.load.table,
+  y = batcount,
+  by = c("species")
+)
+
+View(count_fungal)
+# this keeps only the rows in bat count
+# we don't have a row for substrate, because we don't have
+# a count for substrate
+
+## Pivoting
+# https://tidyr.tidyverse.org/articles/pivot.html
+#sometimes our datasets are not in the format we want for an analysis
+
+head(batdat_count)
+# right now, species is in long format
+# but we could imagine wanting to test the abundance of one species 
+# and how that influences another
+# for example, does the number of MYLU influence the number of MYSE?
+# for that, we would need to make columns if each species count
+# with a row for a site and a date
+
+#pivot_wider is how we take long data, and make it wide
+
+batcounts.wide<- batcount %>% 
+  #this says - make a new df called batcounts.wide using bat counts
+  pivot_wider(names_from = species, 
+              values_from = count
+              ) 
+##make columns for each of the values in the species column and fill those columns with values from the count column
+
+# when we perform this, it automatically fills missing with NAs
+# but we know that missing actually mean 0
+
+batcounts.wide = batcount %>% 
+  pivot_wider(
+  names_from = species, 
+  values_from = count,
+  values_fill = 0
+)
+
+# more often, we might be working in the opposite direction
+# IMO, a more common issue is that programs output SO much
+# information in columns that we would rather have in rows
+
+batcounts.long = batcounts.wide %>% 
+  pivot_longer(
+    cols = c("EPFU","MYLU","MYSE","PESU"), 
+    names_to =   "species",
+    values_to = "count"
+  )
+
+View(batcounts.long)
+# there are frequently more than one way to specify the same thing
+
+#option 2
+batcounts.long = batcounts.wide %>% 
+  pivot_longer(
+    cols = c(starts_with("MY"), "EPFU", "PESU"),
+    names_to =   "species",
+    values_to = "count"
+  )
+View(batcounts.long)
+
+#option 3
+batcounts.long = batcounts.wide %>% 
+  pivot_longer(
+    cols = -c(site, date),
+    names_to =   "species",
+    values_to = "count"
+  )
+View(batcounts.long)
+
+#all do the same thing!
---
+++ old/le2_data_manipulation.R
@@ -0,0 +1,144 @@
+#le2_data_manipulation  
+
+#load important packages##
+library(tidyverse)
+library(tidyr)
+library(dplyr)
+
+#using the bat data - read
+batdat=read.csv("bat_data.csv") # you will need to have the bat data file in the directory you are using
+
+head(batdat)  
+unique(batdat$species)
+hist(batdat$gd)
+
+batdat$lgdL=log10(batdat$gdL)#log the amount of fungus
+batcounts<-aggregate(count~species+site+date,data=batdat, FUN=mean)  #make a df of bat counts
+
+
+##how to use base R's match##
+#imagine I want to match in average fungal loads to my batcounts dataframe
+batloads<-aggregate(lgdL~species+site+date,data=batdat, FUN=mean)  #make a df of bat loads
+
+#create a unique row id - in this case it is the species, site, and date which denote the entries we want to match
+batloads$unique.row.id = paste(batloads$species,batloads$site,batloads$date)
+batcounts$unique.row.id = paste(batcounts$species,batcounts$site,batcounts$date)
+#dataframe you are bringing to first, and the one you matching from second
+batloads$count = batcounts$count[match(batloads$unique.row.id,batcounts$unique.row.id)]
+
+View(batloads)
+
+#can merge together but duplicates columns
+batwide=merge(batloads,batcounts,by=c("site","date"))
+head(batwide)
+#now we have a dataframe with columns for counts and for loads
+
+# Look at some example data that comes with the tidyr package
+# "New' way using 'pivot_wider'
+
+batcounts.wide<- batcounts %>% #this says - make a new df called batcounts.wide using bat counts
+  pivot_wider(names_from = species, values_from = count) 
+##make columns for each of the values in the species column and fill those columns with values from the count column
+
+
+fish_encounters
+
+#let's say we want a new df where each row is a station
+fish_encounters %>%
+  pivot_wider(names_from = station, values_from = seen)
+
+#we didn't include 0's at stations where we didn't observe fish so we can substitute those in
+# Fill in missing values
+fish_encounters %>%
+  pivot_wider(
+    names_from = station, #make the column names based on the unique values of the station column
+    values_from = seen, #fill in these columns based on data in the 'seen' column
+    values_fill = list(seen = 0) #if there isn't a value in seen, give it a zero (the fish wasn't observed)
+  )
+
+#what if we have a wide dataframe and want to make it "tidy"
+head(relig_income) #welp this is awful
+
+relig_income %>%
+  pivot_longer(-religion, names_to = "income", values_to = "count") 
+#the minus sign says don't include the column religion
+#make all of the other column names rows in a column called 'income'
+#make another column called 'count' and associate the values in each of the income classes with that count
+#now you have a variable income and a count of the number of people that were in that income class
+
+head(billboard)
+#this is a dataset of songs and their ranking
+
+billboard %>% 
+  pivot_longer(
+    cols = starts_with("wk"), #take all the cols that start with 'wk' 
+    names_to = "week", #stack them in a column called "week"
+    values_to = "rank",#the values that were in those columns before should be stored in column called rank
+    values_drop_na = TRUE #get rid of the NAs - it means they weren't on the chart. Alternatively could be '101'
+  )
+
+#we probably want week to be an integer, not a character
+billboard %>% 
+  pivot_longer (
+    cols = starts_with("wk"), 
+    names_to = "week", 
+    names_prefix = "wk",
+    values_to = "rank",
+    values_drop_na = TRUE
+  )
+#we can also smush our bat data back together
+head(batcounts.wide)
+
+#remove the batcounts.wide unique row id column
+batcounts.wide$unique.row.id=NULL 
+
+#we want both site and date to be columns
+batcounts.wide %>%
+  pivot_longer(-c(site,date), names_to = "species", values_to = "count")
+
+
+## Group by, Mutate, and Summarise
+batdat %>% 
+  group_by(species) %>% 
+  summarise(mean.fungal.loads=mean(lgdL,na.rm=TRUE))
+#this gives you a summary table, it doesn't change batdat
+
+#if you want to call this table something you would need to assign it
+fungal.load.table = batdat %>% 
+  group_by(species) %>% 
+  summarise(mean.fungal.loads=mean(lgdL,na.rm=TRUE))
+
+fungal.load.table
+
+##Summarise versus Mutate
+batdat_with_sample_size = batdat %>% 
+  #create a new dataframe  called batdat_with_sample_size
+  group_by(site,species,date) %>% 
+  #you can group_by multiple things
+  mutate(sample.size=n())
+#this adds a column to the dataframe
+batdat_with_sample_size
+
+##we could have also just added this column to batdat
+batdat = batdat %>% 
+  #create a new dataframe  called batdat_with_sample_size
+  group_by(site,species,date) %>% 
+  #you can group_by multiple things
+  mutate(sample.size=length(swab_id))
+#this adds a column to the dataframe
+batdat
+
+###some old code with spread and gather - ignore unless this speaks to you###
+#starting with the "old" way - using spread
+batcounts.wide<-spread(batcounts, species,count,convert=T) #spread the dataframe
+
+head(batcounts)
+head(batcounts.wide)
+
+batloads<-aggregate(lgdL~species+site+date,data=batdat, FUN=mean)
+head(batloads)
+
+batloads.wide<-spread(batloads, species,lgdL,convert=T)
+head(batloads.wide)
+
+
